c87ffebfe7bcae214b077d6e914ff3ab9f7b2acf
Jamie <5964236+jamsinclair@users.noreply.github.com>
2024-06-12 02:42:18 +0000

    configure rayon and parallel feature

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@
 #[macro_use]
 extern crate quick_error;
 
+#[cfg(feature = "parallel")]
+extern crate rayon;
+
+#[cfg(not(feature = "parallel"))]
+mod rayon;
+
 use imagequant::{Attributes, Image, QuantizationResult};
 use imgref::*;
 use rgb::*;
@@ -33,6 +39,7 @@ mod encoderust;
 use crossbeam_channel::{Receiver, Sender};
 use std::borrow::Cow;
 use std::io::prelude::*;
+use rayon::prelude::*;
 
 type DecodedImage = CatResult<(ImgVec<RGBA8>, f64)>;
 
